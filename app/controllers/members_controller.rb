7605529b5894d8bceed41e13701554bb8f58f85e
ne280225 <ne280225@senshu-u.jp>
2018-01-23 16:42:37 +0900

    会員情報編集出来るようにするとこまで

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -30,10 +30,17 @@ end
 
 
   def edit
+    @member = Member.find(params[:id])
   end
 
 
   def update
+    @member = Member.find(params[:id])
+		if @member.update(member_params)
+			redirect_to member_path
+		else
+			render 'edit'
+		end
   end
 
 
